6c02112228a7be00093f3b91e07a8d7bc08fdb74
choudhariprathmesh <prathmeshchoudhari07@gmail.com>
2023-03-19 20:06:09 +0530

    Wikipedia Search API Assignment
    Wikipedia Search API Assignment

--- angularapp/src/app/aboutus/aboutus.component.ts
+++ angularapp/src/app/aboutus/aboutus.component.ts
@@ -1,20 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { WikipediaApiService } from '../services/wikipedia.service';
+import { Component, OnInit } from "@angular/core";
+import { WikipediaApiService } from "../services/wikipedia.service";
 
 @Component({
-  selector: 'app-aboutus',
-  templateUrl: './aboutus.component.html',
-  styleUrls: ['./aboutus.component.css']
+  selector: "app-aboutus",
+  templateUrl: "./aboutus.component.html",
+  styleUrls: ["./aboutus.component.css"],
 })
 export class AboutusComponent implements OnInit {
-
+  
   results: any[];
-  constructor(private wikipediaApi: WikipediaApiService) { }
 
-  ngOnInit() {
-  }
+  constructor(private wikipediaApi: WikipediaApiService) {}
+  ngOnInit() {}
   search(searchTerm: string) {
-    this.wikipediaApi.search(searchTerm).subscribe(response => {
+    this.wikipediaApi.search(searchTerm).subscribe((response) => {
       this.results = response.query.search;
     });
   }
